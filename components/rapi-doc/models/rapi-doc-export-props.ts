eb0f02c09c665ae4347441f851589fcba0aee59b
hieucd04 <hieucd04@gmail.com>
2022-04-16 23:26:23 +0700

    SRU

--- components/rapi-doc/models/rapi-doc-export-props.ts
+++
@@ -1,4 +0,0 @@
-import {ComponentExportProps} from "@miniskylab/antimatter-component";
-import {RapidDocComponentProps} from "./rapi-doc-component-props";
-
-export type RapiDocExportProps = ComponentExportProps<RapidDocComponentProps, null>;
