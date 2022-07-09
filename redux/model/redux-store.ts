213d982a0aa9b467ec3743f5828518c4e95f3cba
hieucd04 <hieucd04@gmail.com>
2022-07-09 18:22:33 +0700

    SRU

--- redux/model/redux-store.ts
+++ redux/model/redux-store.ts
@@ -1,5 +1,5 @@
+import {composeWithDevTools} from "@redux-devtools/extension";
 import {AnyAction, applyMiddleware, createStore, PreloadedState, Store} from "redux";
-import {composeWithDevTools} from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import {createPlainObjectActionMiddleware} from "../middlewares";
 
