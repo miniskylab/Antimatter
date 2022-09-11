bc60a40222f8aa6efbc8c243f471a953d60d7955
hieucd04 <hieucd04@gmail.com>
2022-09-11 12:50:54 +0700

    SRU

--- topbar/component.tsx
+++ topbar/component.tsx
@@ -1,3 +1,4 @@
+import {Icon} from "@miniskylab/antimatter-icon";
 import {Icomoon} from "@miniskylab/antimatter-icon/collection/icomoon";
 import {bem} from "@miniskylab/antimatter-model";
 import React from "react";
@@ -7,18 +8,24 @@ import {TopbarProps} from "./model";
  * <p style="color: #9B9B9B; font-style: italic">(no description available)</p>
  */
 export function Topbar({
-    className,
-    functionalityIcons = [],
-    notificationIcons = []
+    className
 }: TopbarProps): JSX.Element
 {
     return (
         <div className={bem(className)}>
-            <div className={`${bem(className, "Logo")} ${Icomoon.Flag}`}/>
-            <div className={bem(className, "NotificationArea")}>
-                {notificationIcons.map((icon, index) => <div key={index} className={bem(className, "Icon")}>{icon}</div>)}
+            <Icon className={bem("Topbar-Logo")} name={Icomoon.Flag}/>
+            <div className={bem(className, "Container")}>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.Sun}/>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.Sun}/>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.Sun}/>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.Sun}/>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.Sun}/>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.Sun}/>
             </div>
-            {functionalityIcons.map((icon, index) => <div key={index} className={bem(className, "Icon")}>{icon}</div>)}
+            <div className={bem(className, "Container", "Right")}>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.Download}/>
+                <Icon className={bem("Topbar-Icon")} name={Icomoon.User}/>
+            </div>
         </div>
     );
 }
