1bc0e884a4af16faa9870d2cce783351d467ef40
hieucd <hieucd04@gmail.com>
2024-07-08 09:13:28 +0700

    SRU

--- components/date-picker/main.tsx
+++ components/date-picker/main.tsx
@@ -16,7 +16,7 @@ import {DatePickerContext, DatePickerProps} from "./models";
 import * as Variant from "./variants";
 
 /**
- * <p style="color: #9B9B9B; font-style: italic">(no description available)</p>
+ * A component that allows users to input date.
  */
 export function DatePicker({
     style = Variant.Default,
--- components/date-picker/models/props.ts
+++ components/date-picker/models/props.ts
@@ -6,7 +6,7 @@ import {type DatePickerStyle} from "./style";
 export class DatePickerProps extends ComponentProps<DatePickerStyle>
 {
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Specify the selected date.
      */
     @IsDate()
     @IsOptional()
@@ -14,7 +14,7 @@ export class DatePickerProps extends ComponentProps<DatePickerStyle>
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Specify the text that will be displayed before input has been entered.
      */
     @IsString()
     @IsOptional()
@@ -22,7 +22,7 @@ export class DatePickerProps extends ComponentProps<DatePickerStyle>
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Specify the way the selected date is displayed.
      *
      * @type DateFormat
      */
@@ -32,7 +32,7 @@ export class DatePickerProps extends ComponentProps<DatePickerStyle>
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Set this option to ***true*** to open the calendar allowing users to select a date from.
      */
     @IsBoolean()
     @IsOptional()
@@ -40,7 +40,7 @@ export class DatePickerProps extends ComponentProps<DatePickerStyle>
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Set this option to ***false*** to prevent the date picker from receiving focus.
      */
     @IsBoolean()
     @IsOptional()
@@ -48,7 +48,7 @@ export class DatePickerProps extends ComponentProps<DatePickerStyle>
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Set this option to ***true*** to specify that the date picker should automatically get focused when it is mounted.
      */
     @IsBoolean()
     @IsOptional()
@@ -56,7 +56,7 @@ export class DatePickerProps extends ComponentProps<DatePickerStyle>
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Set this option to ***false*** to prevent users from editing the entered text in the date picker.
      */
     @IsBoolean()
     @IsOptional()
@@ -64,13 +64,13 @@ export class DatePickerProps extends ComponentProps<DatePickerStyle>
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Specify the piece of code that will be executed when users press the addon to open the calendar.
      */
     readonly onAddonPress?: () => void;
 
 
     /**
-     * <i style="color: #9B9B9B">(not available)</i>
+     * Specify the piece of code that will be executed when the selected date changes.
      */
     readonly onSelectedDateChange?: (newlySelectedDate: Date | undefined) => void;
 }
