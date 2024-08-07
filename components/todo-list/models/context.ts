6f30a19275ac3ca1f99cacf0b3fe83a88d4c0036
hieucd <hieucd04@gmail.com>
2024-08-07 16:15:32 +0700

    SRU

--- components/todo-list/models/context.ts
+++ components/todo-list/models/context.ts
@@ -1,7 +1,7 @@
 import {type ComponentContext} from "@miniskylab/antimatter-framework";
 import {createContext} from "react";
 import {TodoListProps} from "./props";
-import {TodoListState} from "./state";
+import {type TodoListState} from "./state";
 
 export const TodoListContext = createContext<TodoListContext>(undefined);
 export type TodoListContext = ComponentContext<TodoListProps, TodoListState>;
