82f71cca52274c9e7f8950d96b4eb4e680c8a02c
hieucd <hieucd04@gmail.com>
2024-10-20 10:37:07 +0700

    SRU

--- components/todo-list/index.ts
+++ components/todo-list/index.ts
@@ -1,3 +1,6 @@
+export {Reminder} from "./components";
+export {SelectedReminder} from "./classes";
+export type {ReminderChangeData} from "./types";
 export {TodoListProps, type TodoListRef, type TodoListStyle} from "./models";
 export {TodoList} from "./main";
 export {TodoListContextHook} from "./hooks";
